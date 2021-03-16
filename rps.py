8c233a1c5685a219ff020018e1319ca6a00ef50a
Jessica-Thomas <jessica.thomas648@gmail.com>
2021-03-16 18:45:06 -0400

    Game complete and runs

--- rps.py
+++ rps.py
@@ -20,4 +20,37 @@ def get_computer_selection():
     action = Action(selection)
     return action
 
+# compare user and computer choice to decide who wins
+def determine_winner(user_action, computer_action):
+    if user_action == computer_action:
+        print(f"You both selected {user_action.name}. It's a tie!")
+    elif user_action == Action.Rock:
+        if computer_action == Action.Scissors:
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif user_action == Action.Paper:
+        if computer_action == Action.Rock:
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user_action == Action.Scissors:
+        if computer_action == Action.Paper:
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
 
+while True:
+    try:
+        user_action = get_user_selection()
+    except ValueError as e:
+        range_str = f"[0, {len(Action) - 1}]"
+        print(f"Invalid selection. Enter a value in range {range_str}")
+        continue
+
+    computer_action = get_computer_selection()
+    determine_winner(user_action, computer_action)
+
+    play_again = input("Play again? (y/n): ")
+    if play_again.lower() != "y":
+        break
